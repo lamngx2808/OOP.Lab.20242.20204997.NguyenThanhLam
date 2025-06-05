cffa858c161c0df0cfb8bf4beeff5b1ac58f5b59
NguyenThanhLam <81542586+lamngx2808@users.noreply.github.com>
2025-06-05 22:12:41 +0700

    Delete AimsProject directory

--- AimsProject/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"Project","l":"Aims"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Project","l":"Cart"},{"p":"Project","l":"DigitalVideoDisc"}];updateSearchResults();
--- AimsProject/src/hust/soict/dsai/aims/media/Playable.java
+++
@@ -1,7 +0,0 @@
-package AimsProject.src.hust.soict.dsai.aims.media;
-
-import AimsProject.src.hust.soict.dsai.aims.exception.PlayerException;
-
-public interface Playable {
-    public void play() throws PlayerException;
-}
